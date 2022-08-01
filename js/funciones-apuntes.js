c33937e422937a2e53c7366e1f38a0338f4f3d6f
Ector Rivadeneyra <rivadeneyra.ector@gmail.com>
2022-08-01 14:11:06 -0500

    .

---
+++ js/funciones-apuntes.js
@@ -0,0 +1,5 @@
+function descubreLaHerramienta(){
+    document.getElementById('herramienta-popular').style.display='block'
+};
+
+//PUEDO PONER EL PARALLAX CON UN DISPLAY EN FALSE Y QUE LA FUNCTION LO PONGA EN TRUE
